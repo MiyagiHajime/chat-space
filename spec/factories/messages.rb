21bac92aa857c04f400e148f3294f46853f7c57f
hajime miyagi <hajime.prog@gmail.com>
2020-07-28 01:47:55 +0900

    controllerテスト

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
 #messageモデルのダミーインスタンスの作成
-  factory :messages do
+  factory :message do
     content {Faker::Lorem.sentence}
     image   {File.open("#{Rails.root}/public/images/test_image.jpg")}
     user
